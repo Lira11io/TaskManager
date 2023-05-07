21ddc1901b46abc27a8be9ad132990cea744c5bd
Lira11io <Lira11pm@mail.ru>
2023-05-08 01:08:36 +0300

    немного поправлены стили, доработан js: убран дублирующийся код, исправлен id чекбоксов, добавлена проверка на пустоту поля ввода задач

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 //Получаем элементы на странице
 const taskInput = document.querySelector(".manager__task_input"); //поле ввода задачи
 const taskButton = document.querySelector(".manager__task_button"); //кнопка "Добавить"
-const listInput = document.querySelector(".manager__list_input"); //поле списка задач
 const listButton = document.querySelector(".manager__list_button"); //кнопка "Очистить список задач"
 const listItem = document.querySelector(".manager__list_item"); //блок списка задач
 
 //добавляем обработчик события на кнопку "Добавить"
 taskButton.addEventListener("click", () => {
-  const oldInput = document.querySelector(".manager__list_input"); // получаем элемент с классом "manager__list_input"
-  if (oldInput) {
-    // проверяем, есть ли такой элемент вообще
-    oldInput.remove(); // если есть, удаляем старый элемент перед добавлением нового
+  //const oldInput = document.querySelector(".manager__list_input");
+  const listInput = document.querySelector(".manager__list_input"); // получаем элемент с классом "manager__list_input"
+  if (listInput) {
+    // проверяем, есть ли элемент с классом "manager__list_input"
+    listInput.remove(); // если есть, удаляем старый элемент перед добавлением нового
   }
+  //проверяем поле ввода задачи на пустоту
+  if (taskInput.value.trim() === "") {
+    //если поле ввода задачи пустое или состоит только из пробелов
+    return; // прекратить выполнение функции
+  }
+
   //добавляем задачу в список задач
   listItem.innerHTML += `<div class="manager__list_item_input"> 
     <p class="manager__list_item_text">
     ${taskInput.value}
     </p>  
     <label for="checkbox"> 
-    <input class="manager__list_item_checkbox" type="checkbox" name="ready" id="checkbox">
+    <input class="manager__list_item_checkbox" type="checkbox" name="ready" id="checkbox-${Math.random()}">
     </label>
     </div> `;
 
